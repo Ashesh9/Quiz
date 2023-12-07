82f51b57a9ab52e15afe9cc00e2317f2e16e6aaf
Ashesh9 <149551472+Ashesh9@users.noreply.github.com>
2023-12-06 23:01:53 -0500

    set results declaration and reset button

--- j1.js
+++ j1.js
@@ -1,9 +1,17 @@
-var questions = document.getElementById("#play");
+var ask = document.querySelector("#play");
 var playbtn = document.querySelector("#Start");
-var q = document.getElementById("#play");
+var q = document.querySelector("#play");
 var isWin = false;
 var timerElement = document.querySelector("#counter");
+var loseDeclare = document.querySelector("#declare");
+var lose = document.querySelector("#loss");
+var win = document.querySelector("#win");
+var winDeclare = document.querySelector("#declare");
+var resetButton = document.querySelector("#reset");
+var defDeclare = document.querySelector("#declare");
 
+var winCounter = 0;
+var loseCounter = 0;
 var questions = [
   "How much is 2+4:",
   "How much is 20-3:",
@@ -19,12 +27,12 @@ var rightanswers = ["6", "17", "15", "25"];
 
 function renderQuestions() {
   var q = questions[Math.floor(Math.random * questions.length)];
-  console.log(q);
+  ask.value = q;
   return q;
 }
 function startGame() {
   isWin = false;
-  timerCount = 20;
+  timerCount = 5;
   // Prevents start button from being clicked when round is in progress
   playbtn.disabled = true;
   renderQuestions();
@@ -53,9 +61,43 @@ function startTimer() {
   }, 1000);
 }
 
+function loseGame() {
+  loseDeclare.textContent = "GAME OVER";
+  loseCounter++;
+  playbtn.disabled = false;
+  setLosses();
+}
+// Updates lose count on screen and sets lose count to client storage
+function setLosses() {
+  lose.textContent = loseCounter;
+  localStorage.setItem("loseCount", loseCounter);
+}
+
+function winGame() {
+  winDeclare.textContent = "YOU WON!!!🏆 ";
+  winCounter++;
+  playbtn.disabled = false;
+  setWins();
+}
+function setWins() {
+  win.textContent = winCounter;
+  localStorage.setItem("winCount", winCounter);
+}
 //function Quiz() {
 //var problem = renderQuestions();
 //var xyz = document.querySelector("#play");
 //xyz.textContent = problem;
 //}
 playbtn.addEventListener("click", startGame);
+
+function resetGame() {
+  // Resets win and loss counts
+  winCounter = 0;
+  loseCounter = 0;
+  defDeclare.textContent = "...";
+  // Renders win and loss counts and sets them into client storage
+  setWins();
+  setLosses();
+}
+// Attaches event listener to button
+resetButton.addEventListener("click", resetGame);
