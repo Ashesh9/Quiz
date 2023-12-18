da27f72a5a65a6806b24bd87222b06a4cd9d13e4
Ashesh9 <149551472+Ashesh9@users.noreply.github.com>
2023-12-17 23:42:50 -0500

    restructure render logic

--- logic.js
+++ logic.js
@@ -9,63 +9,37 @@ var win = document.querySelector("#win");
 var winDeclare = document.querySelector("#declare");
 var resetButton = document.querySelector("#reset");
 var defDeclare = document.querySelector("#declare");
-var options = document.querySelector("#answer");
+var option0 = document.querySelector("#answer0");
+var option1 = document.querySelector("#answer1");
+var option2 = document.querySelector("#answer2");
+var option3 = document.querySelector("#answer3");
 
 var winCounter = 0;
 var loseCounter = 0;
 
-var questions = [
-  "How much is 2+4:",
-  "How much is 20-3:",
-  "How much is 3*5:",
-  "How much is 50/2:",
-];
-var answers = [
-  "6",
-  "3",
-  "5",
-  "2",
-  "10",
-  "9",
-  "12",
-  "17",
-  "15",
-  "13",
-  "14",
-  "18",
-  "25",
-  "21",
-  "22",
-  "3",
-];
-var answer1 = ["6", "3", "5", "2"];
-var answer2 = ["10", "9", "12", "17"];
-var answer3 = ["15", "13", "14", "18"];
-var answer4 = ["25", "21", "22", "3"];
+index = [];
+index[0] = ["How much is 2+4:", "6", "3", "5", "2"];
+index[1] = ["How much is 20-3:", "10", "9", "12", "17"];
+index[2] = ["How much is 3*5:", "15", "13", "14", "18"];
+index[3] = ["How much is 50/2:", "25", "21", "22", "3"];
 
-var rightanswers = ["6", "17", "15", "25"];
+const rightanswers = ["6", "17", "15", "25"];
 
 function renderQuestions() {
-  var i = Math.floor(Math.random() * questions.length);
-  var issue = questions[i];
-
-  ask.textContent = issue;
-
-  //program a loop to render options.
-  if ((i = 0)) {
-    for (var j = 0; j < 4; j++) {
-      var x = answers[j];
-      options.textContent = x;
-      console.log(options);
-    }
-  }
-
-  console.log(ask);
+  var i = Math.floor(Math.random() * 4);
+  let x = [];
+  let queset = [];
+  queset = x.concat(index[i]);
+  ask.textContent = queset[0];
+  option0.textContent = queset[1];
+  option1.textContent = queset[2];
+  option2.textContent = queset[3];
+  option3.textContent = queset[4];
 }
 
 function startGame() {
   isWin = false;
-  timerCount = 10;
+  timerCount = 5;
   // Prevents start button from being clicked when round is in progress
   playbtn.disabled = true;
   renderQuestions();
@@ -123,7 +97,7 @@ function resetGame() {
   // Resets win and loss counts
   winCounter = 0;
   loseCounter = 0;
-  defDeclare.textContent = "...";
+  defDeclare.textContent = "Good Luck";
   // Renders win and loss counts and sets them into client storage
   setWins();
   setLosses();
